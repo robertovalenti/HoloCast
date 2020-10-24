05b8ae0e1469407ebf44271ab3529541e9c57bac
Roberto Valenti <roberto@sightcorp.com>
2020-10-24 13:28:05 +0200

    Initial commit

---
+++ ui/web-dev.plugins.mjs
@@ -0,0 +1,29 @@
+import { fromRollup } from '@web/dev-server-rollup';
+import rollupCommonjs from '@rollup/plugin-commonjs';
+import rollupBuiltins from 'rollup-plugin-node-builtins';
+import rollupReplace from '@rollup/plugin-replace';
+
+const replace = fromRollup(rollupReplace);
+const builtins = fromRollup(rollupBuiltins);
+const commonjs = fromRollup(rollupCommonjs);
+
+export default [
+  replace({
+    global: 'window',
+    'process.env.NODE_ENV': '"production"',
+    'process.env.CONDUCTOR_URL': process.env.CONDUCTOR_URL
+      ? `"${process.env.CONDUCTOR_URL}"`
+      : 'undefined',
+  }),
+  builtins(),
+  commonjs({
+    include: [
+      'node_modules/fast-json-stable-stringify/**/*',
+      'node_modules/zen-observable/**/*',
+      'node_modules/graphql-tag/**/*',
+      'node_modules/isomorphic-ws/**/*',
+      'node_modules/@msgpack/**/*',
+      'node_modules/@holochain/conductor-api/**/*',
+    ],
+  }),
+];
